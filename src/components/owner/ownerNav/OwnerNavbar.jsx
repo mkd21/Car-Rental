19bf8654fe94521c64fe1be0454b89c8d0012f87
Mayank Deep <rishusinha525@gmail.com>
2025-07-22 22:42:35 +0530

    added owner navnar

--- src/components/owner/ownerNav/OwnerNavbar.jsx
+++ src/components/owner/ownerNav/OwnerNavbar.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom";
-import { assets , dummyUserData } from "../../../assets/all-assets/assets.js";
+import { assets, dummyUserData } from "../../../assets/all-assets/assets.js";
 
 
 export default function OwnerNavbar() {
@@ -8,21 +8,22 @@ export default function OwnerNavbar() {
 
     return (
         <>
-            <section className="relative z-50 flex justify-around items-center bg-light max-xl:h-[4rem] xl:h-[8vh] border-b-2 border-b-[#C1C1C180]
+            <section>
+                <div className="relative z-50 flex justify-around items-center bg-light max-xl:h-[4rem] xl:h-[8vh] border-b-2 border-b-[#C1C1C180]
                 xl:justify-between xl:px-[6rem] ">
 
-                {/* LOGO */}
-                <div className="max-md:w-[30%]" >
-                    <Link to="/">
-                        <img src={assets.logo} alt="company logo" />
-                    </Link>
-                </div>
+                    {/* LOGO */}
+                    <div className="max-md:w-[30%]" >
+                        <Link to="/">
+                            <img src={assets.logo} alt="company logo" />
+                        </Link>
+                    </div>
 
-                <div className=" font-outfit font-[600] text-[#64748B] text-[0.8rem] xl:text-[1.2rem] " >
-                    <p>{` Welcome, ${dummyUserData.name}`}</p>
+                    <div className=" font-outfit font-[600] text-[#64748B] text-[0.8rem] xl:text-[1.2rem] " >
+                        <p>{` Welcome, ${dummyUserData.name}`}</p>
+                    </div>
                 </div>
             </section>
-
         </>
     );
 }
