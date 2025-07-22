19f7ed709eb0ed0132137c499c432c12fc879360
Mayank Deep <rishusinha525@gmail.com>
2025-07-23 02:02:59 +0530

    added initial structure

--- src/components/owner/DashBoard.jsx
+++ src/components/owner/DashBoard.jsx
@@ -1,11 +1,22 @@
 
+import { dummyDashboardData, assets } from "../../assets/all-assets/assets.js";
+
+// booking related data 
+
+const bookingRelatedData = [
+    { Label: "Total Cars", LabelCount: dummyDashboardData.totalCars, LabelIcon: assets.carIconColored },
+    { Label: "Total Bookings", LabelCount: dummyDashboardData.totalBookings, LabelIcon: assets.listIconColored },
+    { Label: "Pending Bookings", LabelCount: dummyDashboardData.pendingBookings, LabelIcon: assets.cautionIconColored },
+    { Label: "Completed Booking", LabelCount: dummyDashboardData.completedBookings, LabelIcon: assets.listIconColored }
+];
+
 
 export default function DashBoard() {
 
     return (
 
         // parent 
-        <div className=" font-outfit bg-yellow-300 w-full p-8 " >
+        <div className=" font-outfit  w-full p-8 " >
 
             <div className=" w-[54%] " >
                 <h1 className=" text-[2rem] font-[400] " >Admin Dashboard</h1>
@@ -13,13 +24,66 @@ export default function DashBoard() {
             </div>
 
             {/* data cards  */}
-            <div>
+            <div className=" flex mt-8" >
+                {
+                    bookingRelatedData.map(iter =>
+                        <div className=" border-2 border-[#DDE1EE] flex justify-around items-center w-[23%] me-3 rounded-md py-4">
+                            {/* details  */}
+                            <div>
+                                <p>{iter.Label}</p>
+                                <p className=" font-[500] text-[1.2rem] " >{iter.LabelCount}</p>
+                            </div>
 
+                            {/* icons  */}
+                            <div className=" w-[16%] bg-[#EEF4FF] rounded-full p-1 " >
+                                <img src={iter.LabelIcon} alt={`${iter.Label} image`} className=" w-full " />
+                            </div>
+                        </div>
+                    )
+                }
             </div>
 
 
-            <div>
+            {/* recent booking and monthly revenue  */}
+            <div className=" bg-indigo-200 flex " >
 
+                {/* recent bookings data  */}
+                <div className=" border-2 border-black "  >
+
+                    <div>
+                        <h3>Recent Bookings</h3>
+                        <p>Latest customer bookings</p>
+                    </div>
+
+                    <div>
+                        {
+                            dummyDashboardData.recentBookings.map(iter =>
+                                <div>
+                                    {/* icon, car model and pickup date  */}
+                                    <div>
+                                        <div>
+                                            <img src={assets.listIconColored} alt="" />
+                                        </div>
+
+                                        <div>
+                                            <p></p>
+                                            <p></p>
+                                        </div>
+
+                                    </div>
+
+                                    {/* amount and booking status  */}
+                                    <div></div>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+
+                {/* total revenue  */}
+                <div className=" border-2 border-black " >
+                    <h3>Monthly Revenue</h3>
+                </div>
             </div>
         </div>
     );
