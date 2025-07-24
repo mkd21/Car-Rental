3c06f8daebf6df36404473cad1c9b69099933b77
Mayank Deep <rishusinha525@gmail.com>
2025-07-25 01:25:04 +0530

    final design

--- src/components/owner/DashBoard.jsx
+++ src/components/owner/DashBoard.jsx
@@ -26,24 +26,26 @@ export default function DashBoard() {
         // parent 
         <div className=" font-outfit  w-full p-8 " >
 
-            <div className=" w-[54%] " >
+            <div className=" xl:w-[54%] " >
                 <h1 className=" text-[2rem] font-[400] " >Admin Dashboard</h1>
                 <p className=" text-[#64748B] font-[400] ">Monitor overall platform performance including total cars, bookings, revenue, and recent activities</p>
             </div>
 
             {/* data cards  */}
-            <div className=" flex mt-8" >
+            <div className=" flex max-md:flex-wrap max-md:justify-between mt-8" >
                 {
                     bookingRelatedData.map(iter =>
-                        <div key={iter.Label} className=" border-2 border-[#DDE1EE] flex justify-around items-center w-[23%] me-3 rounded-md py-4">
+                        <div key={iter.Label} className=" border-2 border-[#DDE1EE] flex justify-around items-center rounded-md py-4
+                            max-md:w-full xl:w-[23%] xl:me-3 max-md:my-3 ">
+
                             {/* details  */}
-                            <div>
-                                <p>{iter.Label}</p>
-                                <p className=" font-[500] text-[1.2rem] " >{iter.LabelCount}</p>
+                            <div className=" max-md:w-[40%] " >
+                                <p className=" text-[#6B7280] " >{iter.Label}</p>
+                                <p className=" font-[500] text-[1.2rem]">{iter.LabelCount}</p>
                             </div>
 
                             {/* icons  */}
-                            <div className=" w-[16%] bg-[#EEF4FF] rounded-full p-1 " >
+                            <div className=" w-[10%] bg-[#EEF4FF] rounded-full p-1 " >
                                 <img src={iter.LabelIcon} alt={`${iter.Label} image`} className=" w-full " />
                             </div>
                         </div>
@@ -53,10 +55,12 @@ export default function DashBoard() {
 
 
             {/* recent booking and monthly revenue  */}
-            <div className="font-inter flex justify-around mt-8 " >
+            <div className="font-inter flex justify-around mt-8 
+                max-md:flex-wrap  " >
 
                 {/* recent bookings data  */}
-                <div className=" border-2 border-[#0000000D] p-4 w-[50%] rounded-lg " style={{ backgroundColor : "0 1px 2px" }} >
+                <div className=" border-2 border-[#0000000D] p-4 w-[50%] rounded-lg 
+                    max-md:w-full max-md:mb-3 " style={{ backgroundColor : "0 1px 2px" }} >
 
                     <div>
                         <h3 className=" text-[1.5rem] " >Recent Bookings</h3>
@@ -79,14 +83,14 @@ export default function DashBoard() {
                                             </div>
 
                                             <div className=" " >
-                                                <p className=" font-[400] text-[1.3rem] " >{`${iter.car.brand} ${iter.car.model}`}</p>
+                                                <p className=" font-[400] xl:text-[1.3rem] " >{`${iter.car.brand} ${iter.car.model}`}</p>
                                                 <p className=" text-[#64748B] text-[0.9rem] " >{` ${month} / ${day} / ${year} `}</p>
                                             </div>
 
                                         </div>
 
                                         {/* amount and booking status  */}
-                                        <div className=" w-[30%] " >
+                                        <div className=" w-[40%] " >
                                             {/* amount  */}
                                             <div>
                                                 <p className=" text-[#64748B] font-[500] " > {`$ ${iter.price}`} </p>
@@ -98,7 +102,6 @@ export default function DashBoard() {
                                             </div>
                                         </div>
                                     </div>
-
                                 )
                             })
                         }
@@ -106,7 +109,8 @@ export default function DashBoard() {
                 </div>
 
                 {/* monthly revenue  */}
-                <div className=" border-2 border-[#0000000D] w-[30%] p-5 rounded-lg " style={{ backgroundColor : "0 1px 2px" }}>
+                <div className=" border-2 border-[#0000000D] w-[30%] p-5 rounded-lg 
+                    max-md:w-full " style={{ backgroundColor : "0 1px 2px" }}>
 
                     <div>
                         <h3 className=" text-[1.3rem] font-[500] " >Monthly Revenue</h3>
